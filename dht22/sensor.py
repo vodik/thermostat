3f5a895669b640eb29239364ec261182ee74ba63
Simon Gomizelj <simon@vodik.xyz>
2017-09-07 01:09:18 -0400

    Add pubsub module

--- dht22/sensor.py
+++ dht22/sensor.py
@@ -3,7 +3,8 @@ import collections
 
 from aioinflux import AsyncInfluxDBClient
 
-from .rbp2 import *  # noqa
+from .rbp2 import dht_read
+from .pubsub import Publisher
 
 
 def read_mock():
@@ -17,30 +18,17 @@ class Sensor:
         self.loop = loop or asyncio.get_event_loop()
 
         self.client = AsyncInfluxDBClient(database='sensors')
-
-        self._data = None
-        self._waiters = collections.deque()
-        self.counter = 0
+        self.publisher = Publisher()
 
     async def poll(self):
-        self.future = self.loop.create_future()
-
         while True:
-            if not self._waiters:
-                self._data = None
-                await asyncio.sleep(2)
-                continue
-
             try:
-                self._data = dht_read(self.pin)
-                # self._data = read_mock()
+                data = dht_read(self.pin)
+                # data = read_mock()
             except RuntimeError:
                 continue
 
-            self.counter += 1
-            for counter, fut in self._waiters:
-                if not fut.done() and (counter is None or counter < self.counter):
-                    fut.set_result((self.counter, self._data))
+            self.publisher.publish(data)
 
             asyncio.ensure_future(self.client.write({
                 'measurement': 'dht22',
@@ -52,42 +40,4 @@ class Sensor:
             await asyncio.sleep(2)
 
     def subscribe(self):
-        return Subscribe(self)
-
-    async def wait(self, *, counter=None):
-        if self._data and counter is not None and self.counter > counter:
-            return self.counter, self._data
-
-        fut = self.loop.create_future()
-        entry = counter, fut
-        self._waiters.append(entry)
-        try:
-            return await fut
-        finally:
-            self._waiters.remove(entry)
-
-    async def read(self):
-        if self._data:
-            return self._data
-
-        fut = self.loop.create_future()
-        entry = 0, fut
-        self._waiters.append(entry)
-        try:
-            _, data = await fut
-            return data
-        finally:
-            self._waiters.remove(entry)
-
-
-class Subscribe:
-    def __init__(self, sensor):
-        self.sensor = sensor
-        self.counter = 0
-
-    async def read(self):
-        while True:
-            counter, data = await self.sensor.wait(counter=self.counter)
-            if counter > self.counter:
-                self.counter = counter
-                return data
+        return self.publisher.subscribe()
